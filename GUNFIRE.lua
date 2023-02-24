bcc0d40015082efb868f5193f36e9206ba12e8a1
takodachi05 <114226597+takodachi05@users.noreply.github.com>
2023-02-25 03:13:58 +0800

    Add files via upload

--- GUNFIRE.lua
+++ GUNFIRE.lua
@@ -1,3 +1,4 @@
+--data=dofile[[D:\a.lua]]
 local set_key1 = 4
 local set_key2 = 5
 local off_key = 6
@@ -5,31 +6,168 @@ local off_key = 6
 ---yeah---
 local recoil_table = {}
 recoil_table["R301"] = {
-    --vertical = {-1,1,-2,1,1,-1,0,-1,-1,-1,0,1,4,3,3,3,1,-1,-2,-2,-2,-2,-2,-2,0,1,-1,3,0},
-    vertical = {-2,0,-2,1,-1    ,0,0,-2,-2,-2    ,-1,3,3,3,3    ,2,1,1,-1,-3     ,-1,-3,-2,-2,-3     ,1,-1,0},
-    --horizontal = {10,10,10,8,7,1,1,1,1,1,2,5,1,1,1,1,1,0},
-    horizontal = {-6,-7,-5,-6,-5    ,-3,-3,-1,-2,2    ,-3,-4,-3,-1,-1    ,1,0,-3,-3,-3    ,0,0,0,0,-1   ,-2,-2,0 },
-    speed = 100,
-    delay = 74,
+    vertical = {-2,-3,
+                -1,-1,
+                -2,-1,
+                1,-1,
+                -1,-1,
+                -1,1,
+                -1,-1,
+                -2,-1,
+                1,-2,
+                -3,-2,
+                -2,-2, 
+                3,2, 
+                2,2, 
+                3,2, 
+                2,2, 
+                1,1, 
+                2,1, 
+                -----------18
+                1,1,
+                1,-1,
+                -----------20
+                1,-1,
+                -2,-2,
+                -2,-2,
+                -2,-1,
+                -2,-2,
+                -----------25
+                -2,-2,
+                -1,-2,
+                2,-1,
+                -----------28
+                0},
+                
+                
+    horizontal = {6,7,
+                  4,4,
+                  3,3,
+                  2,4,
+                  2,3,
+                  1,2,
+                  1,1,
+                  1,1,
+                  0,0,
+                  0,0,
+                  1,1,
+                  3,2,
+                  3,2,
+                  1,-1,
+                  1,-1,
+                  1,-1,
+                  1,-1,
+                  ----------18
+                  1,-1,
+                  1,-1,
+                  -----------20
+                  1,0,
+                  3,2,
+                  1,1,
+                  1,-1,
+                  0,-1,
+                  -----------25
+                  1,-1,
+                  1,-1,
+                  1,1,
+                  -----------28
+                  
+                  
+                  0},
+
+    delay = {74, 148, 222, 296, 370, 444, 519, 593, 667, 741, 815, 889, 963, 1037, 1111, 1185, 1259, 1333, 
+             1407, 1481, 1556, 1630, 1704, 1778, 1852, 1926, 2000,9998},
+
 }  
 recoil_table["flatline"] = {
-    --vertical = {3,0,4,4,4,4  ,-5,-8,-1,-1,-2  ,3,4,0,6,2,2,2,4,0},
-    vertical = {3,0,3,2,2,2  ,-2,-5,-1,-1,-2  ,3,4,0,6,2,1,1,4,0},
-    horizontal = {10,10,10,10,10,5  ,1,1,1,3,3  ,1,1,1,1,1,8,4,4,0},
-    speed = 100,
-    delay = 100,
+    vertical = {3,4,
+                1,0,
+                5,5,
+                4,3,
+                2,1,
+                -1,1,
+                -3,-3,--SHOT 8
+                -4,-4, 
+                -2,-2,--200
+                -2,-2,
+                -4,1,
+                4,3,
+                6,5,
+                4,3,
+                6,5,
+                2,2,
+                -1,1,
+                0,1,
+                4,3, 
+                
+                3,3,--打完20執行這行
+                2,1,
+                -1,1,
+                -3,-3,
+                -2,-3,
+                
+                -5,-4,
+                -4,-4,
+                -4,-5,
+                
+                -1,-1,
+                -2,-2,
+                
+                0},
+
+    
+    horizontal = {7,6,
+                  4,3,
+                  5,4,
+                  4,4,
+                  5,3,
+                  3,3,
+                  2,1,
+                  -1,1,
+                  -1,0,
+                  -1,1,
+                  3,2,
+                  1,1,
+                  3,2,
+                  -2,2,
+                  -2,1,
+                  -2,2,
+                  5,4,
+                  5,4,
+                  2,2,
+                  
+                  1,1,--打完20執行這行
+                  1,1,
+                  3,2,
+                  2,2,
+                  2,1,
+                  
+                  1,2,
+                  2,1,
+                  0,-1,
+                  
+                  2,1,
+                  3,2,
+                  
+                  0},
+    delay = {100, 200, 300, 400, 500, 600, 
+             700, 800, 900, 1000, 1100, 1200, 
+             1300, 1400, 1500, 1600, 1700, 1800,
+             1900, 2000, 2100, 2200, 2300, 2400,
+             2500, 2600, 2700, 2800, 2900,9998},
 }
 
-function recoil_value(_weapon,_duration)
-  local step = math.floor(_duration/10) + 1
+function recoil_value(_weapon,_duration,delaycount)
+  local step_delay = delaycount
+  local step = _duration + 1
   if step > #recoil_table[_weapon]["vertical"] then
         step = #recoil_table[_weapon]["vertical"]
   end
   local weapon_ver = recoil_table[_weapon]["vertical"][step]
-  local weapon_hor = recoil_table[_weapon]["horizontal"][1]
-  local weapon_interval = recoil_table[_weapon]["speed"] 
-  local weapon_delay = recoil_table[_weapon]["delay"]
-  return weapon_ver,weapon_hor,weapon_interval,weapon_delay
+  local weapon_hor = recoil_table[_weapon]["horizontal"][step]
+  local weapon_delay = recoil_table[_weapon]["delay"][step_delay]
+  --local weapon_delay = recoil_table[_weapon]["delay"]
+  return weapon_ver,weapon_hor,weapon_delay
   --return recoil_table[_weapon]["vertical"][step],recoil_table[_weapon]["horizontal"][step],recoil_table[_weapon]["speed"] 
 end
 
@@ -45,10 +183,9 @@ function keybinds(arg)
 end
 
 function OnEvent(event, arg)
-  OutputLogMessage("Event: "..event.."Arg: "..arg.."\n")
+--  OutputLogMessage("Event: "..event.."Arg: "..arg.."\n")
   
   if(event == "PROFILE_ACTIVATED") then
-    OutputLogMessage("Lua script start~\n")
     EnablePrimaryMouseButtonEvents(true)
     current_weapon = "flatline"
     
@@ -59,94 +196,38 @@ function OnEvent(event, arg)
   
   if(event == "MOUSE_BUTTON_PRESSED") then
     keybinds(arg)
-    OutputLogMessage("weapon:"..current_weapon.."\n\n")
+--    OutputLogMessage("weapon:"..current_weapon.."\n\n")
   end
   if (IsKeyLockOn("capslock") )then
     if IsMouseButtonPressed(3) then
       repeat
         if IsMouseButtonPressed(1) then
-        OutputLogMessage("This script has been running for: %d ms", GetRunningTime())
-            shoot_duration = 0.0
+        --OutputLogMessage("This script has been running for: %d ms", GetRunningTime())
+            shoot_duration = 0
+            delay_count = 0
+            delaytime = 0
+            last_time = GetRunningTime()
+            
             repeat
-                ver,hor,interval,delaytime = recoil_value(current_weapon,shoot_duration)
-                MoveMouseRelative(ver,hor)
-                Sleep(delaytime)
-                shoot_duration = shoot_duration + (interval / 10)
-
+            --OutputLogMessage("This script has been running for: %d ms", GetRunningTime())
+                if(GetRunningTime()-last_time>=delaytime) then
+                    delay_count = delay_count+1
+                    for i = 1,2 do
+                     --OutputLogMessage("This script has been running for: %d ms \n", GetRunningTime())
+                      Sleep(15)
+                      ver,hor,delaytime = recoil_value(current_weapon,shoot_duration,delay_count)
+                      MoveMouseRelative(ver,hor)
+                      shoot_duration = shoot_duration + 1
+                    end
+                else
+                    --OutputLogMessage("waiting %d ms \n", GetRunningTime())
+                    Sleep(20)
+                end
             until not IsMouseButtonPressed(1)
-            OutputLogMessage("This script has been running for: %d ms", GetRunningTime())
+            --OutputLogMessage("This script has been running for: %d ms", GetRunningTime())
         end
       until not IsMouseButtonPressed(3)
       
     end
   end
 end
-
-
-
-
-
---[[
-function float_f()
-  if (IsKeyLockOn("capslock") )then
-    if IsMouseButtonPressed(1) then
-      --if(float_count == 1 and float_boolen == true)then
-        Sleep(50)
-        MoveMouseRelative(3,10)--1
-        Sleep(99)
-        MoveMouseRelative(0,2)--2
-        Sleep(99)
-        MoveMouseRelative(4,10)--3
-        Sleep(99)
-        MoveMouseRelative(4,10)--4
-        Sleep(99)
-        MoveMouseRelative(4,7)--5
-        Sleep(99)
-        MoveMouseRelative(4,7)--6
-        Sleep(99)
-        float_count = float_count + 1
-    --  elseif(float_count == 2 and float_boolen == true) then
-        MoveMouseRelative(-5,1)--7
-        Sleep(99)
-        MoveMouseRelative(-8,1)--8
-        Sleep(99)
-        MoveMouseRelative(-5,1)--9
-        Sleep(99)
-        MoveMouseRelative(-5,3)--10
-        Sleep(99)
-        MoveMouseRelative(-5,3)--11
-        Sleep(99)
-        float_count = float_count + 1
-    --  elseif(float_count == 3 and float_boolen == true) then
-        MoveMouseRelative(6,1)--12
-        Sleep(99)
-        MoveMouseRelative(6,1)--13
-        Sleep(99)
-        MoveMouseRelative(6,1)--14
-        Sleep(99)
-        MoveMouseRelative(10,1)--15
-        Sleep(99)
-        MoveMouseRelative(6,10)--16
-        Sleep(99)
-        MoveMouseRelative(6,10)--17
-        Sleep(99)
-        MoveMouseRelative(6,7)--18
-        Sleep(99)
-        MoveMouseRelative(6,7)--19
-        Sleep(99)
-        float_count = 0
-    --  end
-    end
-  end
-end]]
-
---[[repeat
-              MoveMouseRelative(5,1)
-              Sleep(15)
-              MoveMouseRelative(-5,1)
-              Sleep(15)
-              --[[MoveMouseRelative(5,0) 
-              Sleep(15)
-              MoveMouseRelative(-5,0)
-              Sleep(15)
-            until not IsMouseButtonPressed(1)]]
